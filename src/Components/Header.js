f91e98278a7f45dda1d6c139012e2be1e10bee70
alison-grow <agrowz98@gmail.com>
2023-01-28 20:16:39 -0800

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <div className="header-container">
+            <div className="header-title">
+                LANA DEL RANKED
+            </div>
+        </div>
+    )
+}
